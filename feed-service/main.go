81ad5c30c2e91d5aa0fb15ca7ff1ee1fe5c01e6c
Hugo Eduardo Carvajalino Solano <link_2618@hotmail.com>
2023-12-09 12:06:20 -0500

    Feed services is created

---
+++ feed-service/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"cqrs/initial/database"
+	"cqrs/initial/events"
+	"cqrs/initial/repository"
+
+	"github.com/gorilla/mux"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	PostgresDB       string `envconfig:"POSTGRES_DB"`
+	PostgresUser     string `envconfig:"POSTGRES_USER"`
+	PostgresPassword string `envconfig:"POSTGRES_PASSWORD"`
+	NatsAddress      string `envconfig:"NATS_ADDRESS"`
+}
+
+func newRouter() (router *mux.Router) {
+	router = mux.NewRouter()
+	router.HandleFunc("/feeds", createFeedHandler).Methods(http.MethodPost)
+	return
+}
+
+func main() {
+	var cfg Config
+	err := envconfig.Process("", cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	addr := fmt.Sprintf("postgres://%s:%s@postgres/%s?sslmode=disable", cfg.PostgresUser, cfg.PostgresPassword, cfg.PostgresDB)
+	repo, err := database.NewPostgresRepository(addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	repository.SetRepository(repo)
+
+	// Nats
+	n, err := events.NewNats(fmt.Sprintf("nats://%s", cfg.NatsAddress))
+	if err != nil {
+		log.Fatal(err)
+	}
+	events.SetEventStore(n)
+
+	defer events.Close()
+
+	router := newRouter()
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal(err)
+	}
+
+}
--- go.mod
+++ go.mod
@@ -4,11 +4,14 @@ go 1.21.3
 
 require (
 	github.com/elastic/go-elasticsearch/v7 v7.17.10 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/nats-io/nats.go v1.31.0 // indirect
 	github.com/nats-io/nkeys v0.4.5 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/segmentio/ksuid v1.0.4 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 )
